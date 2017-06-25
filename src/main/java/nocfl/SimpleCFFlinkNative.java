8fe636836eca724f523e3c7402d54f8ed003ebd4
Gabor Gevay <ggab90@gmail.com>
2017-06-25 11:12:58 +0200

    Job for measuring iteration overhead with Flink native iterations.

---
+++ src/main/java/nocfl/SimpleCFFlinkNative.java
@@ -0,0 +1,109 @@
+package nocfl;
+
+import gg.BagOperatorHost;
+import gg.CFLConfig;
+import gg.CFLManager;
+import gg.CondOutputSelector;
+import gg.ElementOrEvent;
+import gg.KickoffSource;
+import gg.PhiNode2;
+import gg.operators.AssertEquals;
+import gg.operators.Bagify;
+import gg.operators.ConditionNode;
+import gg.operators.IncMap;
+import gg.operators.SmallerThan;
+import gg.partitioners2.Random;
+import gg.partitioners2.RoundRobin;
+import gg.util.LogicalInputIdFiller;
+import gg.util.TestFailedException;
+import gg.util.Unit;
+import gg.util.Util;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.RichFlatMapFunction;
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.io.DiscardingOutputFormat;
+import org.apache.flink.api.java.operators.IterativeDataSet;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.IterativeStream;
+import org.apache.flink.streaming.api.datastream.SplitStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.DiscardingSink;
+import org.apache.flink.util.Collector;
+
+import java.util.Arrays;
+
+/**
+ * // BB 0
+ * i = 1
+ * do {
+ *     // BB 1
+ *     i = i + 1
+ * } while (i < 100)
+ * // BB 2
+ * assert i == 100
+ */
+
+public class SimpleCFFlinkNative {
+
+	//private static final Logger LOG = LoggerFactory.getLogger(SimpleCF.class);
+
+	public static void main(String[] args) throws Exception {
+		ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+//		Configuration cfg = new Configuration();
+//		cfg.setLong("taskmanager.network.numberOfBuffers", 16384);
+//		StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironment(40, cfg);
+
+		//env.getConfig().setParallelism(1);
+
+		final int n = Integer.parseInt(args[0]);
+
+
+		Integer[] input = new Integer[]{0};
+
+		DataSet<Integer> in = env.fromCollection(Arrays.asList(input));
+
+		IterativeDataSet<Integer> it = in.iterate(n);
+
+		DataSet<Integer> inced = it.map(new MapFunction<Integer, Integer>() {
+			@Override
+			public Integer map(Integer i) throws Exception {
+				return i + 1;
+			}
+		});
+
+		DataSet<Integer> res = it.closeWith(inced);
+
+		//res.map(new RichFlatMapFunction<>()).setParallelism(1);
+		res.flatMap(new RichFlatMapFunction<Integer, Object>() {
+
+			int cnt = -10;
+
+			@Override
+			public void open(Configuration parameters) throws Exception {
+				cnt = 0;
+			}
+
+			@Override
+			public void close() throws Exception {
+				if (cnt != 1) {
+					throw new TestFailedException("Received " + cnt + " number of elements, instead of 1");
+				}
+			}
+
+			@Override
+			public void flatMap(Integer i, Collector<Object> collector) throws Exception {
+				cnt++;
+				if (i != n) {
+					throw new TestFailedException("Received record " + i + " instead of " + n);
+				}
+			}
+		}).setParallelism(1).output(new DiscardingOutputFormat<>());
+
+		//System.out.println(env.getExecutionPlan());
+		env.execute();
+	}
+}
