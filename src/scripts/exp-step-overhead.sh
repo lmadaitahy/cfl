fed5fa4e06d98558cca5180e56dd939e8a7ab314
Gabor Gevay <ggab90@gmail.com>
2017-06-25 11:12:29 +0200

    Restart with same numMachines on job failure

--- src/scripts/exp-step-overhead.sh
+++ src/scripts/exp-step-overhead.sh
@@ -32,62 +32,85 @@ mkdir -p /tmp/ggevay
 # Let's do a stop-cluster to be sure that we don't have any Flink running.
 # Note: For this reason, the config copied by copy-flink.sh should have all the machines that will be used.
 echo -n "Stopping any remaining Flink cluster. "
-./flink-cfl/bin/stop-cluster.sh >$(mktemp /tmp/ggevay/initial-stop-cluster.XXXXXXXXXXXXXXXXXXXXXX)
+./flink-cfl/bin/stop-cluster.sh >$(mktemp /tmp/ggevay/initial-stop-cluster.XXXXXXX)
 echo "Done."
 
 
-for numMachines in {1..25}
-do
-  echo -n "$numMachines machines. "
+for numMachines in {7..25}; do
+    ok=false
+    while [ $ok = false ]; do
+      echo -n "$numMachines machines. "
 
-  echo -n "$numMachines ">>$resFile
+      echo -n "$numMachines ">>$resFile
 
-  ./create-conf-para.sh $numMachines
+      ./create-conf-para.sh $numMachines
 
-  ./flink-cfl/bin/start-cluster.sh >$(mktemp /tmp/ggevay/start-cluster.XXXXXXXXXXXXXXXXXXXXXX)
-  echo -n "Cluster started. "
-  
-  # give the TMs time to really start up
-  sleep 15
+      ./flink-cfl/bin/start-cluster.sh >$(mktemp /tmp/ggevay/start-cluster.XXXXXX)
+      echo -n "Cluster started. "
+      
+      # give the TMs time to really start up
+      sleep 15
 
-  # warmup jobs
-  echo -n "Warmup jobs"
-  for i in {1..10}
-  do
-    tmpOut=$(mktemp /tmp/ggevay/out.XXXXXXXXXXXXXXXXXXXXXX)
-    set +e
-    ./flink-cfl/bin/flink run -c gg.jobs.SimpleCF /home/ggevay/cfl-1.0-SNAPSHOT.jar 100 &>$tmpOut
-    set -e
-    ./check-cfl-success.sh $tmpOut
-    echo -n .
-    sleep 3
-  done
-  echo -n " "
+      # warmup jobs
+      echo -n "Warmup jobs"
+      for i in {1..10}; do
+        tmpOut=$(mktemp /tmp/ggevay/out.XXXXXXXX)
 
-  # Real jobs
-  echo -n "Real jobs" 
-  for i in {1..7}
-  do
-    tmpOut=$(mktemp /tmp/ggevay/out.XXXXXXXXXXXXXXXXXXXXXX)
-    export rtmarker=ezarealt
-    set +e
-    /usr/bin/time -f ${rtmarker}%e ./flink-cfl/bin/flink run -c gg.jobs.SimpleCF /home/ggevay/cfl-1.0-SNAPSHOT.jar 10000 &>$tmpOut
-    set -e
-    ./check-cfl-success.sh $tmpOut
+        set +e
+        ./flink-cfl/bin/flink run -c gg.jobs.SimpleCF /home/ggevay/cfl-1.0-SNAPSHOT.jar 100 &>$tmpOut
+        ./check-cfl-success.sh $tmpOut
+        if [ $? != 0 ]; then
+            # The Flink job failed. Stop the Flink cluster and try with this numMachines again.
+            echo "Flink job failed. Restarting Flink cluster and trying again."
+            echo -n "restart" >>$resFile
+            ./flink-cfl/bin/stop-cluster.sh >$(mktemp /tmp/ggevay/stop-cluster.XXXXXXXX)
+            continue 2 # Continue 2nd enclosing loop
+        fi
+        set -e
 
-    # Get the execution time from the output
-    ./parse-time.sh $tmpOut >>$resFile
-    echo -n " " >>$resFile
+        echo -n .
+        sleep 4
+      done
+      echo -n " "
 
-    echo -n .
+      # Real jobs
+      echo -n "Real jobs" 
+      for i in {1..7}; do
+        tmpOut=$(mktemp /tmp/ggevay/out.XXXXXXXXX)
+        export rtmarker=ezarealt
 
-    sleep 3
-  done
+        set +e
+        /usr/bin/time -f ${rtmarker}%e ./flink-cfl/bin/flink run -c gg.jobs.SimpleCF /home/ggevay/cfl-1.0-SNAPSHOT.jar 10000 &>$tmpOut
+        ./check-cfl-success.sh $tmpOut
+        if [ $? != 0 ]; then
+            # The Flink job failed. Stop the Flink cluster and try with this numMachines again.
+            echo "Flink job failed. Restarting Flink cluster and trying again."
+            echo -n "restart" >>$resFile
+            ./flink-cfl/bin/stop-cluster.sh >$(mktemp /tmp/ggevay/stop-cluster.XXXXXXX)
+            continue 2 # Continue 2nd enclosing loop
+        fi
+        set -e
 
-  echo >>$resFile  #newline
+        # Get the execution time from the output
+        ./parse-time.sh $tmpOut >>$resFile
+        echo -n " " >>$resFile
 
-  echo
+        echo -n .
+        sleep 4
+      done
 
-  ./flink-cfl/bin/stop-cluster.sh >$(mktemp /tmp/ggevay/stop-cluster.XXXXXXXXXXXXXXXXXXXXXX)
-  sleep 1
+      echo >>$resFile  #newline
+
+      echo
+
+      ./flink-cfl/bin/stop-cluster.sh >$(mktemp /tmp/ggevay/stop-cluster.XXXXXXX)
+      sleep 1
+
+      ok=true
+    done  
 done
+
+# Cut out the first part of those lines that have "restart" in them
+tmpResult=$(mktemp /tmp/ggevay/result.XXXXXXX)
+cat $resFile |sed "s/.*restart//" >$tmpResult
+cp $tmpResult $resFile
