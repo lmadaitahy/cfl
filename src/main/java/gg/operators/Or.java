509ecae522126e046ac7b1dbbbad71ed6389f9ad
Gabor Gevay <ggab90@gmail.com>
2017-07-16 18:19:28 +0200

    Make Or short-circuit

--- src/main/java/gg/operators/Or.java
+++ src/main/java/gg/operators/Or.java
@@ -6,23 +6,32 @@ package gg.operators;
 public class Or extends BagOperator<Boolean, Boolean> {
 
     private boolean result;
+    private boolean sent = false;
 
     @Override
     public void openOutBag() {
         super.openOutBag();
         result = false;
+        sent = false;
     }
 
     @Override
     public void pushInElement(Boolean e, int logicalInputId) {
         super.pushInElement(e, logicalInputId);
         result = result || e;
+        if (result && !sent) {
+            sent = true;
+            out.collectElement(true);
+        }
     }
 
     @Override
     public void closeInBag(int inputId) {
         super.closeInBag(inputId);
-        out.collectElement(result);
+        if (!sent) {
+            assert !result;
+            out.collectElement(false);
+        }
         out.closeBag();
     }
 }
