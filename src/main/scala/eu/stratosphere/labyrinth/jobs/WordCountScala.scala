d72d59b2b920cc9f6317e035acdd310bec79cb37
Lorand Madai-Tahy <madai-tahy@campus.tu-berlin.de>
2018-02-12 12:12:53 +0100

    added groupBy

--- src/main/scala/eu/stratosphere/labyrinth/jobs/WordCountScala.scala
+++ src/main/scala/eu/stratosphere/labyrinth/jobs/WordCountScala.scala
@@ -13,71 +13,73 @@ import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 
 object WordCountScala {
 
-  private val stringSerializer = TypeInformation.of(classOf[String]).createSerializer(new ExecutionConfig)
+	private val stringSerializer = TypeInformation.of(classOf[String]).createSerializer(new ExecutionConfig)
 
-  private val tuple2StringIntegerSerializer = TypeInformation.of(new TypeHint[Tuple2[String, Integer]]() {}).createSerializer(new ExecutionConfig)
+	private val tuple2StringIntegerSerializer = TypeInformation.of(new TypeHint[Tuple2[String, Integer]]() {}).createSerializer(new ExecutionConfig)
 
-  @throws[Exception]
-  def main(args: Array[String]): scala.Unit = {
-    val env = StreamExecutionEnvironment.getExecutionEnvironment
-    CFLConfig.getInstance.terminalBBId = 0 // this will have to be found automatically
+	@throws[Exception]
+	def main(args: Array[String]): scala.Unit = {
+		val env = StreamExecutionEnvironment.getExecutionEnvironment
+		CFLConfig.getInstance.terminalBBId = 0 // this will have to be found automatically
 
-    val kickoffSource = new KickoffSource(0) // this as well
-    env.addSource(kickoffSource).addSink(new DiscardingSink[Unit]) // kickoff source has no output
+		val kickoffSource = new KickoffSource(0) // this as well
+		env.addSource(kickoffSource).addSink(new DiscardingSink[Unit]) // kickoff source has no output
 
-    val para = env.getParallelism
-    val lines = List("foo bar foo foo bar lol lol lol foo rofl",
-      " lol foo lol bar lol bar bar foo foo rofl foo",
-      "foo bar foo foo bar lol lol lol foo rofl lasagne")
+		val para = env.getParallelism
+		val lines = List("foo bar foo foo bar lol lol lol foo rofl",
+			" lol foo lol bar lol bar bar foo foo rofl foo",
+			"foo bar foo foo bar lol lol lol foo rofl lasagne")
 
-    // source to read line by line
-    val input = new LabySource[String](env.fromCollection(lines).javaStream, 0, TypeInformation.of(new TypeHint[ElementOrEvent[String]]() {}))
+		// source to read line by line
+		val input = new LabySource[String](env.fromCollection(lines).javaStream, 0, TypeInformation.of(new TypeHint[ElementOrEvent[String]]() {}))
 
-    val split = new LabyNode[String, String](
-      "split2",
-      LabyWrap.flatMap((s: String, coll: BagOperatorOutputCollector[String]) => for(elem <- s.split(" ")) { coll.collectElement(elem) }),
-      0,
-      new RoundRobin[String](para),
-      stringSerializer,
-      TypeInformation.of(new TypeHint[ElementOrEvent[String]]() {})
-    )
-      .addInput(input, true)
-      .setParallelism(para)
+		val split = new LabyNode[String, String](
+			"split2",
+			LabyWrap.flatMap((s: String, coll: BagOperatorOutputCollector[String]) => for(elem <- s.split(" ")) { coll.collectElement(elem) }),
+			0,
+			new RoundRobin[String](para),
+			stringSerializer,
+			TypeInformation.of(new TypeHint[ElementOrEvent[String]]() {})
+		)
+		  .addInput(input, true)
+		  .setParallelism(para)
 
-    val mapnode = new LabyNode[String, Tuple2[String, Integer]](
-      "map-phase",
-      LabyWrap.map((s: String) => new Tuple2[String, Integer](s, 1)),
-      0,
-      new RoundRobin[String](para),
-      stringSerializer,
-      TypeInformation.of(new TypeHint[ElementOrEvent[Tuple2[String, Integer]]]() {})
-    )
-      .addInput(split, true, false)
-      .setParallelism(para)
+		val mapnode = new LabyNode[String, Tuple2[String, Integer]](
+			"map-phase",
+			LabyWrap.map((s: String) => new Tuple2[String, Integer](s, 1)),
+			0,
+			new RoundRobin[String](para),
+			stringSerializer,
+			TypeInformation.of(new TypeHint[ElementOrEvent[Tuple2[String, Integer]]]() {})
+		)
+		  .addInput(split, true, false)
+		  .setParallelism(para)
 
-    // count phase
-    val reduceNode = new LabyNode[Tuple2[String, Integer], Tuple2[String, Integer]](
-      "reduce-phase",
-      new GroupByString0Count1,
-      0,
-      new Always0[Tuple2[String, Integer]](para),
-      tuple2StringIntegerSerializer,
-      TypeInformation.of(new TypeHint[ElementOrEvent[Tuple2[String, Integer]]]() {})
-    )
-      .addInput(mapnode, true, false)
-      .setParallelism(para)
+		// count phase
+		val reduceNode = new LabyNode[Tuple2[String, Integer], Tuple2[String, Integer]](
+			"reduce-phase",
+			LabyWrap.groupBy((old: Integer, current: Integer) => old + current),
+			0,
+			new Always0[Tuple2[String, Integer]](para),
+			tuple2StringIntegerSerializer,
+			TypeInformation.of(new TypeHint[ElementOrEvent[Tuple2[String, Integer]]]() {})
+		)
+		  .addInput(mapnode, true, false)
+		  .setParallelism(para)
 
-    val printNode = new LabyNode[Tuple2[String, Integer], Unit](
-      "print-phase", new Print[Tuple2[String, Integer]]("printcount"),
-      0, new Always0[Tuple2[String, Integer]](1),
-      tuple2StringIntegerSerializer,
-      TypeInformation.of(new TypeHint[ElementOrEvent[Unit]]() {})
-    )
-      .addInput(reduceNode, true, false)
-      .setParallelism(1)
+		val printNode = new LabyNode[Tuple2[String, Integer], Unit](
+			"print-phase",
+			new Print[Tuple2[String, Integer]]("printcount"),
+			0,
+			new Always0[Tuple2[String, Integer]](1),
+			tuple2StringIntegerSerializer,
+			TypeInformation.of(new TypeHint[ElementOrEvent[Unit]]() {})
+		)
+		  .addInput(reduceNode, true, false)
+		  .setParallelism(1)
 
-    LabyNode.translateAll()
-    System.out.println(env.getExecutionPlan)
-    env.execute
-  }
+		LabyNode.translateAll()
+		System.out.println(env.getExecutionPlan)
+		env.execute
+	}
 }
--- src/main/scala/eu/stratosphere/labyrinth/operators/LabyWrap.scala
+++ src/main/scala/eu/stratosphere/labyrinth/operators/LabyWrap.scala
@@ -1,6 +1,11 @@
 package eu.stratosphere.labyrinth.operators
 
+import java.util
+import java.util.HashMap
+
 import eu.stratosphere.labyrinth.BagOperatorOutputCollector
+import org.apache.flink.api.java.tuple
+import org.apache.flink.api.java.tuple.Tuple2
 
 object LabyWrap {
 
@@ -20,6 +25,38 @@ object LabyWrap {
 			override def pushInElement(e: IN, logicalInputId: Int): Unit = {
 				super.pushInElement(e, logicalInputId)
 				f(e, out)
+			}
+		}
+	}
+
+	def groupBy[LEFT,RIGHT](f: (RIGHT, RIGHT) => RIGHT): GroupBy[LEFT,RIGHT] = {
+
+		new GroupBy[LEFT,RIGHT]() {
+			override def openOutBag(): Unit = {
+				super.openOutBag()
+				hm = new util.HashMap[LEFT, RIGHT]
+			}
+
+			override def pushInElement(e: tuple.Tuple2[LEFT, RIGHT], logicalInputId: Int): Unit = {
+				super.pushInElement(e, logicalInputId)
+				val g = hm.get(e.f0)
+				if (g == null) {
+					hm.put(e.f0, e.f1)
+				} else {
+					hm.replace(e.f0, f(g, e.f1))
+				}
+			}
+
+			override def closeInBag(inputId: Int): Unit = {
+				super.closeInBag(inputId)
+
+				import scala.collection.JavaConversions._
+
+				for (e <- hm.entrySet) {
+					out.collectElement(Tuple2.of(e.getKey, e.getValue))
+				}
+				out.closeBag()
+				hm = null
 			}
 		}
 	}
